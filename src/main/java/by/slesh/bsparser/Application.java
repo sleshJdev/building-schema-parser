2f66a89b657ebaf0d916b8831f7b0dcd9e521421
sleshJdev <slesh.eugene93@gmail.com>
2015-05-29 00:32:09 +0300

    initial commit: added opencv dependencies.

---
+++ src/main/java/by/slesh/bsparser/Application.java
@@ -0,0 +1,12 @@
+package by.slesh.bsparser;
+
+
+/**
+ * @author Eugene Putsykovich(slesh) May 28, 2015
+ */
+public class Application {
+
+        public static void main(String[] args) {
+                System.out.println("4");
+        }
+}
